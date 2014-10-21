a126a8b5b32ce3125e0727ff1906176e68b470ad
Alexander Larsson <alexl@redhat.com>
2014-10-21 16:32:55 +0200

    Really add gthree/gthree.h

---
+++ gthree/gthree.h
@@ -0,0 +1,23 @@
+#ifndef __GTHREE_H__
+#define __GTHREE_H__
+
+#define __GTHREE_H_INSIDE__
+
+#include <gthree/gthreetypes.h>
+#include <gthree/gthreeenums.h>
+#include <gthree/gthreearea.h>
+#include <gthree/gthreebasicmaterial.h>
+#include <gthree/gthreecamera.h>
+#include <gthree/gthreeface.h>
+#include <gthree/gthreegeometry.h>
+#include <gthree/gthreematerial.h>
+#include <gthree/gthreemesh.h>
+#include <gthree/gthreemultimaterial.h>
+#include <gthree/gthreeobject.h>
+#include <gthree/gthreerenderer.h>
+#include <gthree/gthreescene.h>
+#include <gthree/gthreetexture.h>
+
+#undef __GTHREE_H_INSIDE__
+
+#endif /* __GTHREE_H__ */
