76b7455633c1d205bce45a1a3f613af182643fc1
Alexander Larsson <alexl@redhat.com>
2026-04-05 00:30:58 +0200

    Drop pmrem-* tests
    The plan it to do automatic pmrem generation instead.

--- tests/test-pbr.c
+++ tests/test-pbr.c
@@ -1,7 +1,6 @@
 #include <math.h>
 #include "test-common.h"
 #include <gthree/gthreemeshphysicalmaterial.h>
-#include <gthree/gthreepmremgenerator.h>
 
 static void
 add_envmap_lights (GthreeScene *scene)
@@ -308,85 +307,6 @@ test_standard_gold_metallic (GthreeScene **scene, GthreeCamera **camera)
     }
 }
 
-static GthreeScene *pmrem_scene = NULL;
-
-static void
-pmrem_renderer_setup (GthreeRenderer *renderer)
-{
-  if (!pmrem_scene)
-    return;
-
-  g_autoptr(GthreePMREMGenerator) gen = gthree_pmrem_generator_new (renderer);
-  g_autoptr(GthreeCubeTexture) cube_tex = test_cube_texture_colored ();
-  GthreeTexture *pmrem_tex = gthree_pmrem_generator_from_cubemap (gen, cube_tex);
-
-  g_autoptr(GList) meshes = gthree_object_find_by_type (GTHREE_OBJECT (pmrem_scene), GTHREE_TYPE_MESH);
-  for (GList *l = meshes; l; l = l->next)
-    {
-      GthreeMesh *mesh = l->data;
-      GthreeMaterial *mat = gthree_mesh_get_material (mesh, 0);
-      if (GTHREE_IS_MESH_STANDARD_MATERIAL (mat))
-        gthree_mesh_standard_material_set_env_map (GTHREE_MESH_STANDARD_MATERIAL (mat), pmrem_tex);
-    }
-  g_object_unref (pmrem_tex);
-}
-
-static void
-test_pmrem_metalness (GthreeScene **scene, GthreeCamera **camera)
-{
-  *scene = gthree_scene_new ();
-  pmrem_scene = *scene;
-  *camera = GTHREE_CAMERA (gthree_perspective_camera_new (45, 4.0/3.0, 0.1, 100));
-  gthree_object_set_position_xyz (GTHREE_OBJECT (*camera), 0, 0, 3.5);
-  gthree_object_add_child (GTHREE_OBJECT (*scene), GTHREE_OBJECT (*camera));
-
-  add_envmap_lights (*scene);
-
-  g_autoptr(GthreeGeometry) geom = gthree_geometry_new_sphere (0.4, 32, 16);
-
-  float metalness[] = { 0.0, 0.3, 0.7, 1.0 };
-  for (int i = 0; i < 4; i++)
-    {
-      g_autoptr(GthreeMeshStandardMaterial) mat = gthree_mesh_standard_material_new ();
-      graphene_vec3_t red;
-      gthree_mesh_standard_material_set_color (mat, graphene_vec3_init (&red, 0.8, 0.2, 0.2));
-      gthree_mesh_standard_material_set_metalness (mat, metalness[i]);
-      gthree_mesh_standard_material_set_roughness (mat, 0.3);
-
-      GthreeMesh *mesh = gthree_mesh_new (geom, GTHREE_MATERIAL (mat));
-      gthree_object_set_position_xyz (GTHREE_OBJECT (mesh), (i - 1.5) * 1.0, 0, 0);
-      gthree_object_add_child (GTHREE_OBJECT (*scene), GTHREE_OBJECT (mesh));
-    }
-}
-
-static void
-test_pmrem_roughness (GthreeScene **scene, GthreeCamera **camera)
-{
-  *scene = gthree_scene_new ();
-  pmrem_scene = *scene;
-  *camera = GTHREE_CAMERA (gthree_perspective_camera_new (45, 4.0/3.0, 0.1, 100));
-  gthree_object_set_position_xyz (GTHREE_OBJECT (*camera), 0, 0, 3.5);
-  gthree_object_add_child (GTHREE_OBJECT (*scene), GTHREE_OBJECT (*camera));
-
-  add_envmap_lights (*scene);
-
-  g_autoptr(GthreeGeometry) geom = gthree_geometry_new_sphere (0.4, 32, 16);
-
-  float roughness[] = { 0.0, 0.3, 0.6, 1.0 };
-  for (int i = 0; i < 4; i++)
-    {
-      g_autoptr(GthreeMeshStandardMaterial) mat = gthree_mesh_standard_material_new ();
-      graphene_vec3_t silver;
-      gthree_mesh_standard_material_set_color (mat, graphene_vec3_init (&silver, 0.9, 0.9, 0.9));
-      gthree_mesh_standard_material_set_metalness (mat, 1.0);
-      gthree_mesh_standard_material_set_roughness (mat, roughness[i]);
-
-      GthreeMesh *mesh = gthree_mesh_new (geom, GTHREE_MATERIAL (mat));
-      gthree_object_set_position_xyz (GTHREE_OBJECT (mesh), (i - 1.5) * 1.0, 0, 0);
-      gthree_object_add_child (GTHREE_OBJECT (*scene), GTHREE_OBJECT (mesh));
-    }
-}
-
 void
 register_pbr_tests (void)
 {
@@ -399,6 +319,4 @@ register_pbr_tests (void)
   register_test ("physical-iridescence", test_physical_iridescence);
   register_test ("physical-ior", test_physical_ior);
   register_test ("standard-gold-metallic", test_standard_gold_metallic);
-  register_test_full ("pmrem-metalness", test_pmrem_metalness, pmrem_renderer_setup);
-  register_test_full ("pmrem-roughness", test_pmrem_roughness, pmrem_renderer_setup);
 }
