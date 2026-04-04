d6fbd723ba1d206609fc34faf86e5d8b5bc30c60
Alexander Larsson <alexl@redhat.com>
2026-04-04 16:40:42 +0200

    Better default light intensities in properties example

--- examples/properties.c
+++ examples/properties.c
@@ -107,19 +107,19 @@ init_scene (void)
 
   ambient_light = gthree_ambient_light_new (red ());
   gthree_object_add_child (GTHREE_OBJECT (scene), GTHREE_OBJECT (ambient_light));
-  gthree_light_set_intensity (GTHREE_LIGHT (ambient_light), G_PI);
+  gthree_light_set_intensity (GTHREE_LIGHT (ambient_light), 0.2);
 
   geometry_light = gthree_geometry_new_sphere (4, 8, 8);
   material_light = gthree_mesh_basic_material_new ();
   gthree_mesh_basic_material_set_color (material_light, green ());
 
-  point_light = gthree_point_light_new (green (), 1 * G_PI, 0);
+  point_light = gthree_point_light_new (green (), 60000, 0);
   gthree_object_add_child (GTHREE_OBJECT (scene), GTHREE_OBJECT (point_light));
 
   particle_light = gthree_mesh_new (geometry_light, GTHREE_MATERIAL (material_light));
   gthree_object_add_child (GTHREE_OBJECT (point_light), GTHREE_OBJECT (particle_light));
 
-  directional_light = gthree_directional_light_new (blue (), 1.2 * G_PI);
+  directional_light = gthree_directional_light_new (blue (), 1.0);
   gthree_object_set_position_xyz (GTHREE_OBJECT (directional_light),
                                   1, 1, -1);
   gthree_object_add_child (GTHREE_OBJECT (scene), GTHREE_OBJECT (directional_light));
